4718412c8336816f28d1c8b97faee4468490dc9f
Akshay Hegde <akshay.hegde17@gmail.com>
2014-12-04 20:27:36 -0800

    zsh: remove man function from styles.zsh

--- .zsh/styles.zsh
+++ .zsh/styles.zsh
@@ -63,16 +63,3 @@ zstyle ':completion:*:history-words' stop yes
 zstyle ':completion:*:history-words' remove-all-dups yes
 zstyle ':completion:*:history-words' list false
 zstyle ':completion:*:history-words' menu yes
-
-# Colorize man pages
-man() {
-      env \
-          LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-          LESS_TERMCAP_md=$(printf "\e[1;31m") \
-          LESS_TERMCAP_me=$(printf "\e[0m") \
-          LESS_TERMCAP_se=$(printf "\e[0m") \
-          LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-          LESS_TERMCAP_ue=$(printf "\e[0m") \
-          LESS_TERMCAP_us=$(printf "\e[1;32m") \
-          man "$@"
-}
