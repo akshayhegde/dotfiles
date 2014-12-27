8a24f6e8692e783a09cc872718ad055567a0e252
Akshay Hegde <akshay.hegde17@gmail.com>
2014-12-26 16:36:02 -0800

    bin: only output two columns

--- .bin/xterm-colors.py
+++ .bin/xterm-colors.py
@@ -2,6 +2,6 @@
 # Display the colors available in a terminal.
 
 for color in range(0, 256):
-    for i in range(0, 10):
+    for i in range(0, 2):
         print("\033[38;5;%sm%03s\033[m" % (str(color), str(color)), end=' ')
     print()
