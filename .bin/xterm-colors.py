95dd25f174c66a0ccc7a38c5a36e5839b5480179
Akshay Hegde <akshay.hegde17@gmail.com>
2014-12-26 21:22:51 -0800

    bin: better xterm colors output.

--- .bin/xterm-colors.py
+++ .bin/xterm-colors.py
@@ -2,6 +2,7 @@
 # Display the colors available in a terminal.
 
 for color in range(0, 256):
-    for i in range(0, 2):
+    for i in range(0, 1):
         print("\033[38;5;%sm%03s\033[m" % (str(color), str(color)), end=' ')
-    print()
+    if color % 10 == 0:
+        print()
