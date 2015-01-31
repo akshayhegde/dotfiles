cc9593c601eb40d6938e19169ffdc43fa39afc3e
Akshay Hegde <akshay.hegde17@gmail.com>
2015-01-31 00:14:20 -0800

    tmux: Several changes for now playing script
    - Use bash instead of zsh for script since none of zsh's features are
      used.
    - Handle case when artist may be missing (like when playing iTunes
      U videos)

--- .bin/itunes-current-track-tmux.sh
+++ .bin/itunes-current-track-tmux.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env zsh
-# ajh's iTunes now playing script for tmux
-# Modified from a now playing script I got online.
-# Date Modified: February 23, 2013
+#!/usr/bin/env bash
+# iTunes now playing script for tmux
+
 ITUNES_TRACK=$(osascript <<EOF
 if appIsRunning("iTunes") then
     tell app "iTunes" to get the name of the current track
@@ -13,15 +12,19 @@ end appIsRunning
 EOF)
 
 if test "x$ITUNES_TRACK" != "x"; then
-ITUNES_ARTIST=$(osascript <<EOF
-if appIsRunning("iTunes") then
-    tell app "iTunes" to get the artist of the current track
-end if
+    ITUNES_ARTIST=$(osascript <<EOF
+        if appIsRunning("iTunes") then
+            tell app "iTunes" to get the artist of the current track
+        end if
 
-on appIsRunning(appName)
-    tell app "System Events" to (name of processes) contains appName
-end appIsRunning
-EOF)
+        on appIsRunning(appName)
+            tell app "System Events" to (name of processes) contains appName
+        end appIsRunning
+    EOF)
 
-echo ' | ♪' $ITUNES_TRACK '-' $ITUNES_ARTIST
+    if [ "$ITUNES_ARTIST" == "" ]; then
+        echo ' | ♪' $ITUNES_TRACK
+    else
+        echo ' | ♪' $ITUNES_TRACK '-' $ITUNES_ARTIST
+    fi
 fi
