6aeec956e3157c9cc9d10f153d5ae14079c2dd8b
Akshay Hegde <code@akshay-hegde.com>
2018-12-27 19:39:21 -0800

    Tweak ipython startup settings

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -3,5 +3,5 @@
 c = get_config()
 
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.term_title = False
-c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.term_title = True
+c.TerminalIPythonApp.display_banner = True
