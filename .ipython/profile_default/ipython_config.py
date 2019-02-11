6563b90a542457809e62c69c99a7faebca4829d9
Akshay Hegde <code@akshay-hegde.com>
2019-02-10 22:36:07 -0800

    Tweak IPython settings

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -3,5 +3,9 @@
 c = get_config()
 
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.term_title = True
+c.TerminalInteractiveShell.term_title = False
+c.TerminalInteractiveShell.true_color = True
+c.TerminalInteractiveShell.highlighting_style = 'bw'
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
 c.TerminalIPythonApp.display_banner = True
