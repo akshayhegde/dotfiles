c759e45ed5b9ef78808df463ce53a6c4d2e3c410
Akshay Hegde <code@akshay-hegde.com>
2019-02-11 00:31:11 -0800

    IPython: Tweak colors

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,5 +1,7 @@
 # Configuration file for ipython.
 
+from pygments.token import Token, String, Number, Keyword, Name, Comment
+
 c = get_config()
 
 c.TerminalInteractiveShell.confirm_exit = False
@@ -8,4 +10,21 @@ c.TerminalInteractiveShell.true_color = True
 c.TerminalInteractiveShell.highlighting_style = 'bw'
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
-c.TerminalIPythonApp.display_banner = True
+
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+    Token.Prompt: '#B3B8C4',
+    Token.PromptNum: '#B3B8C4',
+    Token.OutPrompt: '#B3B8C4',
+    Token.OutPromptNum: '#B3B8C4',
+
+    String: '#95B47B noitalic',
+
+    Keyword: '#A57A9E',
+
+    Name.Function: '#CC6666',
+    Name.Builtin:'#CC6666',
+    Name.Class: '#CC6666 nobold',
+    Name.Decorator: '#CC6666 italic',
+
+    Comment: '#515F6A',
+}
